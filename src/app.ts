fc559447860cc6965bfe66e8331d80b341918845
Horquidia <horquidia.a.lima@gmail.com>
2025-07-17 21:58:44 -0300

    Tratamento de exceções

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 
+import { errorHandling } from './middlewares/error-handling';
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(errorHandling)
 
 export { app };
