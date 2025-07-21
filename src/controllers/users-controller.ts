69b583ed59d2b3d8b6bfab4b52a2e6fa8207602a
Horquidia <horquidia.a.lima@gmail.com>
2025-07-21 20:58:34 -0300

    Validando dados do usuário

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,8 +1,18 @@
 import { Request, Response } from "express";
+import {z} from "zod"
+import { UserRole } from "@prisma/client";
 
 class UsersController{
     async create(request: Request, response: Response){
-        response.json({ message: "User created" });
+        const bodySchema = z.object({
+            name: z.string().trim().min(2, {message: "Name must have at least 2 characters."}),
+            email: z.string().trim().email({message: "Invalid email."}).toLowerCase(),
+            password: z.string().trim().min(6,{message: "Password must have at least 6 characters."}),
+            role: z.enum([UserRole.employee, UserRole.manager]).default(UserRole.employee),
+        })
+
+        const { name, email, password, role } = bodySchema.parse(request.body);
+        response.json({ name, email, password, role });
     }
 }
 
