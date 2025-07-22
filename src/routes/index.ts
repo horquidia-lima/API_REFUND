da337a9c104ced07caaa9ecc4d5044329389b8f6
Horquidia <horquidia.a.lima@gmail.com>
2025-07-21 21:37:42 -0300

    Criando controller e rota de autenticação

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
 import { usersRoutes } from "./users-routes";
+import { sessionsRoutes } from "./sessions-routes";
 
 const routes = Router();
 
 //Rotas publicas
 
 routes.use("/users", usersRoutes);
+routes.use("/sessions", sessionsRoutes);
 
 export { routes };
